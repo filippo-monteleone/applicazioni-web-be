04239096014451d48e79b030bb4fb0bc5290f3ce
Filippo Monteleone <136260618+filippo-monteleone@users.noreply.github.com>
2024-07-05 18:35:31 +0200

    changed database

---
+++ ApplicazioniWeb1/Migrations/20240705163507_eigth.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApplicazioniWeb1.Migrations
+{
+    /// <inheritdoc />
+    public partial class eigth : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "CurrentCharge",
+                table: "Books",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<float>(
+                name: "TargetCharge",
+                table: "Books",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<float>(
+                name: "Time",
+                table: "Books",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CurrentCharge",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "TargetCharge",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "Time",
+                table: "Books");
+        }
+    }
+}
